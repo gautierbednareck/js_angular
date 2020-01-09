a5cbb7f00c86d0802fb363b12e65c3780bf11bd3
Bednareck gautier <gautierbednareck@gmail.com>
2020-01-09 14:14:35 +0100

    ajout classe player

---
+++ puissance4/v2/js/player.js
@@ -0,0 +1,25 @@
+class P4 {
+    //selector=balise id selecetionné
+    constructor(pseudo,couleur)
+    {
+        this.pseudo=pseudo;
+        this.couleur=couleur;
+    }
+    getPseudo()
+    {
+        return this.pseudo;
+    }
+    getCouleur()
+    {
+        return this.couleur;
+    }
+    setPseudo(surname)
+    {
+        return this.pseudo=surname;
+    }
+    setCouleur(color)
+    {
+        return this.couleur=color;
+    }
+
+}
